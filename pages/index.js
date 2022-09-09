77c3a47fa5bd78c22b11e0f1411b21ab64587554
Harshit Rai Verma <63351479+Harshit-RV@users.noreply.github.com>
2022-09-09 08:25:05 +0000

    Basic JSX with functions

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,28 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
+
 export default function Home() {
   return (
-    <h1>hey</h1>
+    //<h1>hey</h1>
+    {element}
+    
+    
+    // const root = ReactDOM.createRoot(document.getElementById('root'));
+    //root.render(element);
   )
 }
